e47436aaf4c29e0428e9579716f7f1380b413944
Tanvi Kapurwan <125541005+TanviKapurwan23@users.noreply.github.com>
2025-01-14 00:18:43 +0530

    Add files via upload

---
+++ sqrt.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+
+    cout << sqrt(25);
+    return 0;
+}
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main(){
+//     cout<<sqrt(25);
+//     return 0;
+// }
