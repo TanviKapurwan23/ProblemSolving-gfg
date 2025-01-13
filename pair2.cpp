cb653495cead0e7607d8bbdf893dec44216d9c81
Tanvi Kapurwan <125541005+TanviKapurwan23@users.noreply.github.com>
2025-01-14 00:19:12 +0530

    Add files via upload
    added gfg solutions

---
+++ pair2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<utility>
+
+using namespace std;
+
+int main(){
+
+    pair<string, double> PAIR2("geeksForgeeks", 1.23);
+
+    cout<<PAIR2.first<<" ";
+    cout<<PAIR2.second<<endl;
+
+    return 0;
+}
+
---
+++ withValue.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<utility>
+
+using namespace std;
+
+int main(){
+
+    pair<int, char> PAIR1(12, 'A');
+
+    // PAIR1.first = 100;
+    // PAIR1.second = 'G';
+
+    cout<<PAIR1.first<<endl;
+    cout<<PAIR1.second<<endl;
+
+    return 0;
+}
